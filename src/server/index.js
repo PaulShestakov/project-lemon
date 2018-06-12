be51eb31bca20783e675b73edaeaf515bc11973c
Шестаков Павел <shestakov-pavel@bk.ru>
2018-06-12 18:35:50 +0300

    compression use

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,14 @@
 const http = require('http');
 const path = require('path');
 const express = require('express');
+const compression = require('compression');
 
 
 const port = process.env.PORT || 8080;
 
 const app = express();
+
+app.use(compression());
 
 app.use('/static/', express.static(path.join(__dirname, '../../dist')));
 
